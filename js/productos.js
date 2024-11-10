a783910481d287d57f9941c8894fa356bc9fb7d3
Juani Suso <juanisuso@gmail.com>
2024-11-10 20:43:37 -0300

    estilos2

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 
-function verMasProducto(nombre, imagen, precio) {
-    const producto = { nombre: nombre, imagen: imagen, precio: precio };
+function verMasProducto(nombre, imagen, precio, descripcion) {
+    const producto = { nombre: nombre, imagen: imagen, precio: precio, descripcion: descripcion };
     localStorage.setItem("productoSeleccionado", JSON.stringify(producto));
     window.location.href = "producto.html";
 }

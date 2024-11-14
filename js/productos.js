33dcd2d2c4e8cb2ba716c1af438899434df9fb26
Juani Suso <juanisuso@gmail.com>
2024-11-14 17:22:47 -0300

    arreglo productos

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,5 @@
 function verMasProducto(nombre, imagen, precio, descripcion) {
-    const producto = { nombre: nombre, imagen: imagen, precio: precio, descripcion: descripcion };
+    const producto = { nombre: nombre, imagen: imagen, precio: precio, descripcion: descripcion, };
     localStorage.setItem("productoSeleccionado", JSON.stringify(producto));
     window.location.href = "producto.html";
 }
